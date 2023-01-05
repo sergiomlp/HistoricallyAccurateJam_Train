5d7019a029bcb64139ff22610b22af48c1e39b78
ryochan819 <104522760+ryochan819@users.noreply.github.com>
2023-01-05 16:34:53 +0000

    FactoryUI

---
+++ Assets/Scripts/UI_FactoryRequest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_FactoryRequest : MonoBehaviour
+{
+    [SerializeField] private Image requestUIPrefab;
+    [SerializeField] private Vector3 offset = new Vector3(0, 2f, 0);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
