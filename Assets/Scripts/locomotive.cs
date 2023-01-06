2c843ece96e83c6cfcb265cc80fc80c4c6b2b0d9
ryochan819 <104522760+ryochan819@users.noreply.github.com>
2023-01-06 16:07:07 +0000

    Wagon will always follow train head

--- Assets/Scripts/locomotive.cs
+++ Assets/Scripts/locomotive.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class locomotive : MonoBehaviour
 {
 
-    public float speed=10;
+    public float speed = 10;
     private Rigidbody rb;
     //public GameObject destination;
 
@@ -15,35 +15,57 @@ public class locomotive : MonoBehaviour
 
     GameObject nextDestination;
 
+    [SerializeField] private bool isWagon;
+    [SerializeField] private Transform joint;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody>();
-        
-        
+        rb = GetComponent<Rigidbody>();
+
+
     }
 
     void OnAwake()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        nextDestination=destinationSensor.GetComponent<destinationSensor>().detectedDestination;
-
-        if(nextDestination!=null)
+        if (!isWagon)
         {
-            Vector3 lookPos = nextDestination.transform.position - transform.position;
-            lookPos.y=0;
-            Quaternion rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation=Quaternion.Slerp(transform.rotation,rotation,Time.deltaTime*turningSpeed);
+            nextDestination = destinationSensor.GetComponent<destinationSensor>().detectedDestination;
+
+            if (nextDestination != null)
+            {
+                Vector3 lookPos = nextDestination.transform.position - transform.position;
+                lookPos.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turningSpeed);
 
 
-            rb.velocity=transform.forward*speed;
+                rb.velocity = transform.forward * speed;
+            }
+        }
+        else
+        {
+            float distance = Vector3.Distance(transform.position, joint.position);
+            if (distance > 0.5f)
+            {
+                Vector3 lookPos = joint.position - transform.position;
+                lookPos.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turningSpeed * 0.7f);
+
+                rb.velocity = transform.forward * speed;
+            }
         }
 
+
         
+
+
     }
 }
