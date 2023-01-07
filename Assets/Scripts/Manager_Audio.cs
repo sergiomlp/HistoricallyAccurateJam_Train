06230e7c82f2d122bcc5bc1868895510a3013cd0
ryochan819 <104522760+ryochan819@users.noreply.github.com>
2023-01-07 22:10:57 +0000

    Update Manager_Audio.cs

--- Assets/Scripts/Manager_Audio.cs
+++ Assets/Scripts/Manager_Audio.cs
@@ -7,12 +7,12 @@ public class Manager_Audio : MonoBehaviour
     public static Manager_Audio instance;
     public static Manager_Audio GetInstance() { return instance; }
 
-    public  AudioSource EarnMoney;
-    public  AudioSource Brake;
-    public  AudioSource UpgradeButton;
-    public  AudioSource Crash;
-    public  AudioSource trainHorn;
-    public  AudioSource ResocureGet;
+    public AudioSource EarnMoney;
+    public AudioSource Brake;
+    public AudioSource UpgradeButton;
+    public AudioSource Crash;
+    public AudioSource trainHorn;
+    public AudioSource ResocureGet;
     public AudioSource RequestSound;
 
     private void Start()
