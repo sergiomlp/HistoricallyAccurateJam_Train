26712f7b443059683546d38d842796da0884bc3b
ryochan819 <104522760+ryochan819@users.noreply.github.com>
2023-01-08 15:55:02 +0000

    Tutorial Added

--- Assets/Scripts/UI_Menu.cs
+++ Assets/Scripts/UI_Menu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI_Menu : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class UI_Menu : MonoBehaviour
     [SerializeField] GameObject CreditsActive;
     [SerializeField] GameObject Button;
     [SerializeField] GameObject Background;
+
+    [SerializeField] Sprite[] tutorialImage;
+    [SerializeField] Image tutorialDisplay;
+    private int tutorialIndex = 0;
+
+    [SerializeField] GameObject NextButton;
 
     public void StartBtn()
     {
@@ -22,11 +29,43 @@ public class UI_Menu : MonoBehaviour
         Background.SetActive(false);
     }
 
-    public void TutorialBack()
+    public void TutorialClose()
     {
         tutorialActive.SetActive(false);
         Button.SetActive(true);
         Background.SetActive(true);
+    }
+
+    public void TutorialBack()
+    {
+        if(tutorialIndex==0)
+        {
+            tutorialActive.SetActive(false);
+            Button.SetActive(true);
+            Background.SetActive(true);
+        }
+        else
+        {
+            tutorialIndex--;
+            tutorialDisplay.sprite = tutorialImage[tutorialIndex];
+            if(!NextButton.activeInHierarchy)
+            {
+                NextButton.SetActive(true);
+            }
+        }
+    }
+
+    public void TutorialNext()
+    {
+        if (tutorialIndex < 5)
+        {
+            tutorialIndex++;
+            tutorialDisplay.sprite = tutorialImage[tutorialIndex];
+            if (tutorialIndex == 5)
+            {
+                NextButton.SetActive(false);
+            }
+        }
     }
 
     public void CreditsBtn() 
